4d5d201ec2cb099ad718eb185e5af66677b21f65
paACode <ackermann.pascal@gmail.com>
2025-05-26 08:31:25 +0200

    SQL Script to Create Metabase DB added

---
+++ archive/create_metabase_db.sql
@@ -0,0 +1,31 @@
+-- This DB is used for Metabase Visualizations
+USE football_smart_scout;
+
+/* For Top List the following Views are needed:
+1. view_normalized_player_stat_values --> For Calc of Playerscore 
+2. view:summed_player_stat_values --> To join absolute stats of player
+3. */
+
+
+-- Create materialized Views , like this player_stats table and view_unique_player_stat_values is not needed in this DB
+
+CREATE TABLE mat_view_normalized_player_stat_values AS 
+SELECT * FROM palest_dbm_project.view_normalized_player_stat_values;
+
+CREATE TABLE mat_view_summed_player_stat_values AS 
+SELECT * FROM palest_dbm_project.view_summed_player_stat_values;
+    
+
+/* For Top List the following Tables are needed:
+1. transfer--> to filter by budget and join values in top list
+2. tranfer_value --> to filter by budget and join values in top list
+3. base_data_player --> to join base information about player*/
+
+CREATE TABLE transfer AS SELECT * FROM palest_dbm_project.transfer;
+CREATE TABLE transfer_value AS SELECT * FROM palest_dbm_project.transfer_value;
+CREATE TABLE base_data_players AS SELECT * FROM palest_dbm_project.base_data_players;
+
+-- For the heatmap the player_action_coordinates table is needed
+
+CREATE TABLE player_action_coordinates AS SELECT * FROM palest_dbm_project.player_action_coordinates;
+
